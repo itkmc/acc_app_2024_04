9942138e9766cd4f63b97281eceab9c74f21a1c3
itkmc <itkang0219@gmail.com>
2024-05-08 15:48:04 +0900

    입력된 연 월에 해당하는 날짜 범위 구하기

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/order/repository/OrderItemRepository.java
@@ -1,8 +1,13 @@
 package com.koreait.exam.acc_app_2024_04.app.order.repository;
 
 import com.koreait.exam.acc_app_2024_04.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
+   Page<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
 }
