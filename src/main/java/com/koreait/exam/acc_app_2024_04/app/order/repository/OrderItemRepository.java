4d76abf119bc39a09d898cd892b42263b454277c
itkmc <itkang0219@gmail.com>
2024-05-09 13:12:02 +0900

    정산 데이터 생성

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/order/repository/OrderItemRepository.java
@@ -1,13 +1,11 @@
 package com.koreait.exam.acc_app_2024_04.app.order.repository;
 
 import com.koreait.exam.acc_app_2024_04.app.order.entity.OrderItem;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-   Page<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
+   List<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate);
 }
